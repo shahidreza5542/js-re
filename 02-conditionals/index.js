ed313f4ac67aafef46cd24890fadedc27eb9d17d
shahidreza <shahidreza660@gmail.com>
2025-08-25 11:10:39 +0530

    Add initial HTML and JavaScript files for loop demonstration

---
+++ 02-conditionals/index.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+
+
+let age = 18;
+let grace = 2;
+
+// console.log(17 ** 2);
+// console.log(17 % 2);
+// console.log(15 = 3);
+
+
+
+if(age > 18){
+    console.log("you can drive");
+    
+} else {
+    console.log("you cannot drive");
+    
+}
